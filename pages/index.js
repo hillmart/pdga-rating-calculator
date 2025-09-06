e7b7e10920b9804df5ef503c2a5d05de09e1a55e
hillmart <hillmart@oregonstate.edu>
2025-09-06 14:47:29 -0700

    Initial commit

---
+++ pages/index.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div style={{ padding: "2rem", textAlign: "center" }}>
+      <h1>Hello World</h1>
+      <p>This is a minimal Next.js app deployed on Vercel!</p>
+    </div>
+  );
+}
